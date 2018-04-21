42fdb18e47dfa26ecf1eb79f0c4b0a1157fc1064
grig-koksharov <grig.koksharov@gmail.com>
2018-04-21 11:50:04 +0300

    Added 2 sql scripts for db schema including users table and its starting data

---
+++ database/scripts/DB-data-on-start.sql
@@ -0,0 +1,5 @@
+insert into users (username, password, email, phone) values
+    ('Grigorii', '123', 'admin@admin.ru', '71234567893');
+
+insert into user_roles (username, role) values
+    ('Grigorii', 'Administrator');
