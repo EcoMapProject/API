a0b35e54ded9de63aeda62ccb633f623e64667e0
grig-koksharov <grig.koksharov@gmail.com>
2018-05-14 16:30:50 +0300

    Added json data from RecycleMap in resources directory

--- database/scripts/DB-data-on-start.sql
+++ database/scripts/DB-data-on-start.sql
@@ -4,5 +4,5 @@ insert into users (username, password, email, phone) values
 insert into user_roles (username, role) values
     ('Grigorii', 'Administrator');
 
-insert into bins (name, address, latitude, longitude) values
-    ('Контейнер для пластика', 'ул. Коллонтай, 15к1', '59.9249632', '30.49478');
+--insert into bins (name, address, latitude, longitude) values
+--    ('Контейнер для пластика', 'ул. Коллонтай, 15к1', '59.9249632', '30.49478');
